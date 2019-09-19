21ae6429ecd0ebb1c119c4d2be0f5310ffb5a2ab
Ryan Arpe <ryan_arpe@hotmail.com>
2019-09-19 21:54:35 +0100

    Update qDoc.js

--- src/qDoc.js
+++ src/qDoc.js
@@ -1,13 +1,20 @@
-import config from './qConfig';
-
 const enigma = require('enigma.js');
-const schema = require('enigma.js/schemas/12.20.0.json');
-const SenseUtilities = require('enigma.js/sense-utilities');
+const schemas = require('enigma.js/schemas/12.20.0.json');
+// const SenseUtilities = require('enigma.js/sense-utilities');
 
+const config = {
+  schema: schemas,
+  host: 'localhost',
+  prefix: '',
+  port: '4848',
+  url: 'ws://localhost:4848/app/Cars.qvf',
+  rejectUnauthorized: false,
+  appId: 'Cars.qvf',
+};
 
 async function qDocAsync() {
-  const url = SenseUtilities.buildUrl(config);
-  const session = enigma.create({ schema, url });
+  // const url = SenseUtilities.buildUrl(config);
+  const session = enigma.create(config);
   const global = await session.open();
   const qDoc = await global.openDoc(config.appId);
   return qDoc;
